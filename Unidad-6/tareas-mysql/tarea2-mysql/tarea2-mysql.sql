4d08bd0f5e78e599c41746f67598e79c03d53802
Adrian-89 <adrianif2003@gmail.com>
2024-03-13 19:31:59 +0000

    Se añade Unidad-6

--- Unidad-6/tareas-mysql/tarea2-mysql/tarea2-mysql.sql
+++ Unidad-6/tareas-mysql/tarea2-mysql/tarea2-mysql.sql
@@ -86,6 +86,19 @@
 +----------+------------+-------------+----------+
             **/
             --Mostrar los primeros 5 clientes ordenados por nombre.
+            SELECT nombre FROM clientes  ORDER BY nombre ASC LIMIT 5;
+/**
++------------+
+| nombre     |
++------------+
+| Cliente 1  |
+| Cliente 10 |
+| Cliente 11 |
+| Cliente 12 |
+| Cliente 13 |
++------------+
+**/
+
             --Mostrar los productos con un precio mayor a 50.
             select * from productos where precio > 50;
             /**
@@ -167,8 +180,35 @@ Ninguna coincidencia
             **/
     --Consultas de ejemplo para practicar joins
             --Seleccionar todos los clientes y sus órdenes, incluso si no tienen órdenes
-            
+            SELECT * FROM clientes as c LEFT JOIN ordenes as o on o.id_cliente=c.id_cliente;
+            /**
++------------+------------+---------------+----------+------------+-------------+----------+
+| id_cliente | nombre     | direccion     | id_orden | id_cliente | id_producto | cantidad |
++------------+------------+---------------+----------+------------+-------------+----------+
+|          1 | Cliente 1  | Dirección 1   |        1 |          1 |           1 |        2 |
+|          2 | Cliente 2  | Dirección 2   |        2 |          2 |           2 |        1 |
+|          3 | Cliente 3  | Dirección 3   |        3 |          3 |           3 |        3 |
+|          4 | Cliente 4  | Dirección 4   |        4 |          4 |           4 |        2 |
+|          5 | Cliente 5  | Dirección 5   |        5 |          5 |           5 |        1 |
+|          6 | Cliente 6  | Dirección 6   |        6 |          6 |           6 |        2 |
+|          7 | Cliente 7  | Dirección 7   |        7 |          7 |           7 |        3 |
+|          8 | Cliente 8  | Dirección 8   |        8 |          8 |           8 |        2 |
+|          9 | Cliente 9  | Dirección 9   |        9 |          9 |           9 |        1 |
+|         10 | Cliente 10 | Dirección 10  |       10 |         10 |          10 |        2 |
+|         11 | Cliente 11 | Dirección 11  |       11 |         11 |          11 |        3 |
+|         12 | Cliente 12 | Dirección 12  |       12 |         12 |          12 |        2 |
+|         13 | Cliente 13 | Dirección 13  |       13 |         13 |          13 |        1 |
+|         14 | Cliente 14 | Dirección 14  |       14 |         14 |          14 |        2 |
+|         15 | Cliente 15 | Dirección 15  |       15 |         15 |          15 |        3 |
+|         16 | Cliente 16 | Dirección 16  |       16 |         16 |          16 |        2 |
+|         17 | Cliente 17 | Dirección 17  |       17 |         17 |          17 |        1 |
+|         18 | Cliente 18 | Dirección 18  |       18 |         18 |          18 |        2 |
+|         19 | Cliente 19 | Dirección 19  |       19 |         19 |          19 |        3 |
+|         20 | Cliente 20 | Dirección 20  |       20 |         20 |          20 |        2 |
++------------+------------+---------------+----------+------------+-------------+----------+
+            **/
             --Seleccionar todas las órdenes junto con los productos correspondientes
+            SELECT c.nombre from clientes as c JOIN ordenes as o on o.id_cliente=c.id_cliente JOIN productos as p on p.id_producto=o.id_producto WHERE p.precio > 50;
             --Mostrar el nombre de los clientes que han realizado órdenes de productos que cuestan más de 50
             --Obtener el nombre de los productos que no se han ordenado aún
             --Mostrar el nombre del cliente, el producto y la cantidad para todas las órdenes
